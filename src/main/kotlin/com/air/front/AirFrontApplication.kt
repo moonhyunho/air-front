bc83e99c6aa8afdf9191565a6debba878711085f
moonhyunho <moonh_2@naver.com>
2020-05-29 21:51:55 +0900

    Initial commit

---
+++ src/main/kotlin/com/air/front/AirFrontApplication.kt
@@ -0,0 +1,11 @@
+package com.air.front
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AirFrontApplication
+
+fun main(args: Array<String>) {
+    runApplication<AirFrontApplication>(*args)
+}
---
+++ src/test/kotlin/com/air/front/AirFrontApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.air.front
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AirFrontApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
